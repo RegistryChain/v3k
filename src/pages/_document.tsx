986ec595d631429b6c3b6bb4d0d612740613d900
Michael Manzano <cm172596@gmail.com>
2025-05-28 16:56:19 -0300

    Add google-analytics for content policy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -75,10 +75,14 @@ export default class MyDocument extends Document {
       <Html suppressHydrationWarning>
         <Head>
           {process.env.NODE_ENV === 'production' && (
-            <meta httpEquiv="Content-Security-Policy"
-              content="script-src 'self' 'sha256-...' https://www.googletagmanager.com plausible.io static.cloudflareinsights.com;
-                connect-src 'self' https://www.google-analytics.com;"/>
-
+            <meta
+              httpEquiv="Content-Security-Policy"
+              content="
+                  worker-src 'self';
+                  script-src 'self' 'sha256-UyYcl+sKCF/ROFZPHBlozJrndwfNiC5KT5ZZfup/pPc=' https://*.googletagmanager.com https://www.google-analytics.com plausible.io static.cloudflareinsights.com *.ens-app-v3.pages.dev 'wasm-unsafe-eval';
+                  connect-src 'self' https://www.google-analytics.com;
+                "
+            />
           )}
           {/* eslint-disable-next-line react/no-danger */}
           <script dangerouslySetInnerHTML={{ __html: hiddenCheckScript }} />
